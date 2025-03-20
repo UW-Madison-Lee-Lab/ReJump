69db196bc14285d8673bb6fec336a8e91740c0b4
cychomatica <cychomk0@gmail.com>
2025-03-19 22:21:35 -0500

    update sft script

--- scripts/qwen3b_train_sft_no_reasoning.sh
+++ scripts/qwen3b_train_sft_no_reasoning.sh
@@ -2,15 +2,13 @@ N_GPUS=1
 BASE_MODEL=Qwen/Qwen2.5-3B
 DATA_DIR=datasets/blobs/50_shot/no_reasoning
 ROLLOUT_TP_SIZE=1
-EXPERIMENT_NAME=blobs-qwen2.5-3b
+EXPERIMENT_NAME=blobs-qwen2.5-3b-sft-no-reasoning
 VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -m verl.trainer.fsdp_sft_trainer \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
 data.train_batch_size=32 \
-data.max_prompt_length=256 \
-data.max_response_length=1024 \
 
 trainer.logger=['wandb'] \
 +trainer.val_before_train=False \
@@ -22,4 +20,4 @@ trainer.test_freq=10 \
 trainer.project_name=LIFTR \
 trainer.experiment_name=$EXPERIMENT_NAME \
 trainer.total_epochs=15 2>&1 | tee verl_demo.log
-
+trainer.default_hdfs_dir=~/experiments/liftr/$EXPERIMENT_NAME
