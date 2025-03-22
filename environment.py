28923bd8a93444d3cb77d80becaf5e9b6288a100
cychomatica <cychomk0@gmail.com>
2025-03-21 19:51:34 -0500

    add iterative sft experiments

--- environment.py
+++ environment.py
@@ -1,6 +1,9 @@
 import os
+
 root_dir = os.path.dirname(os.path.abspath(__file__))
+
 WANDB_INFO = {
     'project': 'liftr',
     'entity': 'lee-lab-uw-madison'
 }
+HUGGINGFACE_API_KEY = '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'
