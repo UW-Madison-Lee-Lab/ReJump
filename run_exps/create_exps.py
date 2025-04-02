489f6ebae217bc973b86889cafe7c30a267bef17
Yuchen Zeng <yzeng58@wisc.edu>
2025-04-02 02:40:36 -0500

    update the mixed dataset

--- run_exps/create_exps.py
+++ run_exps/create_exps.py
@@ -19,14 +19,15 @@ parser.add_argument("--shot", type=int, nargs="+", default=shot_list)
 parser.add_argument("--train", action="store_true")
 parser.add_argument("--n_gpus", type=int, default=2)
 parser.add_argument("--response_length_thinking_factor", type=float, default=2.0)
-parser.add_argument("--load_train_step", type=int, default=None)
+parser.add_argument("--load_train_step", type=int, default=0)
 parser.add_argument("--n_samples", type=int, nargs="+", default=[10000])
 parser.add_argument("--noise_level", type=float, nargs="+", default=[None])
 parser.add_argument("--label_flip_rate", type=float, default=0.0)
-parser.add_argument("--plot", action="store_true")
+parser.add_argument("--data_mode", type=str, default="default", choices=["default", "grid", "mixed"])
+parser.add_argument("--wandb", type=int, default=2, choices=[1, 2])
 args = parser.parse_args()
 
-if args.load_train_step is not None:
+if args.load_train_step:
     if len(args.model) > 1:
         raise ValueError("Only one model is supported when loading train step")
     if len(args.dataset) > 1:
@@ -44,19 +45,6 @@ shot_list = args.shot
 n_samples_list = args.n_samples
 noise_level_list = args.noise_level
 
-
-# def eval(
-#     dataset_name,
-#     shot,
-#     model_name,
-#     template_type="qwen-instruct",
-#     response_length=1024
-# ):
-#     return f"""
-# python -m verl.trainer.main_eval \
-#     data.path={get_result_dir(dataset_name, model_name, shot, template_type, response_length)}/test.parquet \
-#     trainer.wandb=True
-#     """
 
 os.makedirs(f"{root_dir}/run_exps/auto", exist_ok=True)
  
@@ -95,7 +83,7 @@ for dataset in dataset_list:
                             num_samples=n_samples,
                             noise_level=noise_level,
                             label_flip_rate=args.label_flip_rate,
-                            plot=args.plot
+                            data_mode=args.data_mode
                         )
                         command_list.append(gen_command)
                         if args.train:
@@ -109,11 +97,12 @@ for dataset in dataset_list:
                                 num_samples=n_samples,
                                 noise_level=noise_level,
                                 label_flip_rate=args.label_flip_rate,
-                                n_gpus=args.n_gpus
+                                n_gpus=args.n_gpus,
+                                data_mode=args.data_mode
                             )
                             command_list.append(train_command)
                         else:
-                            if args.load_train_step is not None:
+                            if args.load_train_step:
                                 model_path = get_model_dir(
                                     dataset_name=dataset,
                                     model_name=model,
@@ -123,6 +112,7 @@ for dataset in dataset_list:
                                     num_samples=n_samples,
                                     noise_level=noise_level,
                                     label_flip_rate=args.label_flip_rate,
+                                    data_mode=args.data_mode,
                                     train_step=args.load_train_step
                                 )
                             else:
@@ -139,18 +129,12 @@ for dataset in dataset_list:
                                 noise_level=noise_level,
                                 label_flip_rate=args.label_flip_rate,
                                 n_gpus=args.n_gpus,
-                                plot=args.plot
+                                data_mode=args.data_mode,
+                                wandb=args.wandb,
+                                train_step=args.load_train_step
                             )
                             command_list.append(inference_command)
-                            # eval_command = eval(
-                            #     dataset_name=dataset,
-                            #     shot=shot,
-                            #     model_name=model_path,
-                            #     template_type=template_type,
-                            #     response_length=response_length
-                            # )
-                            # command_list.append(eval_command)
-                            
+
                         bash_script = "\n".join(command_list)
                         model_name = get_model_name(
                             dataset_name=dataset,
@@ -160,7 +144,8 @@ for dataset in dataset_list:
                             response_length=response_length,
                             num_samples=n_samples,
                             noise_level=noise_level, 
-                            label_flip_rate=args.label_flip_rate
+                            label_flip_rate=args.label_flip_rate,
+                            data_mode=args.data_mode
                         )
                         script_path = f"{root_dir}/run_exps/auto/{model_name}_train_{args.train}.sh"
                         script_paths.append(script_path)
