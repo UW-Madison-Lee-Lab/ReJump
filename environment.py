620cbdbc030d874ca6c203376ff62983a0b8a67b
Shuibai Zhang <shuibai.zhang@outlook.com>
2025-04-05 00:14:49 -0500

    temp

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
  
 OPENAI_API_KEY = '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'
 HUGGINGFACE_API_KEY = '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'
-ANTHROPIC_API_KEY = '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'
+ANTHROPIC_API_KEY = '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'
 GEMINI_API_KEY = {
     'yz': 'AIzaSyDa6IZiztMRZl_j2_em_MjruiCyZs9vEFs',
 }
