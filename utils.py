31b6a6723f37e9c0553262c1264ba07f88e250f5
Yuchen Zeng <yzeng58@wisc.edu>
2025-04-22 00:23:03 -0500

    code for tree converter and benchmark

--- utils.py
+++ utils.py
@@ -71,3 +71,7 @@ def check_results(
 def load_json(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
+    
+def save_json(data, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(data, f)
